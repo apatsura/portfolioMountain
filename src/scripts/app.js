7c2c7d83d7365de84e1f30f188533221bf6f295c
Alexandr Patsura <alexandrpatsura@gmail.com>
2017-09-21 23:22:32 +0300

    done index page

---
+++ src/scripts/app.js
@@ -0,0 +1,16 @@
+(function() {
+  var cards = document.querySelectorAll(".authoriz-btn");
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    clickListener();
+  }
+  
+  function clickListener() {
+    card.addEventListener("click", function() {
+      var block = document.querySelector(".user__card");
+      var c = block.classList;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+    });
+  }
+  
+})();
