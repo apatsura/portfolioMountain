08672a695e78402581e6b0fe37f68de6044fcd05
Alexandr Patsura <alexandrpatsura@gmail.com>
2017-09-24 18:59:06 +0300

    update about

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,10 @@
 (function() {
+  
   var cards = document.querySelectorAll(".authoriz-btn");
   for ( var i = 0, len = cards.length; i < len; i++ ) {
     var card = cards[i];
     clickListener();
+    
   }
   
   function clickListener() {
@@ -29,11 +31,69 @@
     }
   });
   
+  function initMap() {
+    var myGeo = {
+      zoom: 15,
+      center: {lat: 47.781488, lng: 35.186588},
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#ffffff'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#000000'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#000000'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#000000'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#00bfa5'}]
+        },
+      ]
+    }
+    var map = new google.maps.Map(document.getElementById("map"), myGeo);
+    var marker = new google.maps.Marker({
+      position: myGeo.center,
+      map: map
+    });
+  }
+  
 })();
+
 
 
 // loader
 // $(window).load(function() { 
-// 	$(".loader-inner").fadeOut(); 
-// 	$(".loader").delay(400).fadeOut("slow"); 
+//   $(".loader-inner").fadeOut(); 
+//   $(".loader").delay(400).fadeOut("slow"); 
 // });
