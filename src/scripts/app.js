925f0b2f08ba02aaf8dad19c366cf75ff193cccd
Alexandr Patsura <alexandrpatsura@gmail.com>
2018-05-07 22:41:53 +0300

    delete parallax for speed develop

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,27 +1,27 @@
-(function() {
+// (function() {
   
-  // переход к статье через меню в сайдбаре
-  $('.article__item_link').on('click','a', function (event) {
-    // исключаем стандартную реакцию браузера
-    event.preventDefault();
-    // получем идентификатор блока из атрибута href
-    var id  = $(this).attr('href'),
-    // находим высоту, на которой расположен блок
-    top = $(id).offset().top;
-    // анимируем переход к блоку, время: 800 мс
-    $('body,html').animate({scrollTop: top}, 800);
-  });
+//   // переход к статье через меню в сайдбаре
+//   $('.article__item_link').on('click','a', function (event) {
+//     // исключаем стандартную реакцию браузера
+//     event.preventDefault();
+//     // получем идентификатор блока из атрибута href
+//     var id  = $(this).attr('href'),
+//     // находим высоту, на которой расположен блок
+//     top = $(id).offset().top;
+//     // анимируем переход к блоку, время: 800 мс
+//     $('body,html').animate({scrollTop: top}, 800);
+//   });
   
-})();
+// })();
 
-import { parallax } from './common/parallax';
-// import { flipCard } from './common/flipCard';
-// import { toggleMenu } from './common/toggleMenu';
-// import { menuSpy } from './common/menuSpy';
+// import { parallax } from './common/parallax';
+import { flipCard } from './common/flipCard';
+import { toggleMenu } from './common/toggleMenu';
+import { menuSpy } from './common/menuSpy';
 // import { initMap } from './common/initMap';
 
-parallax();
-// flipCard();
-// toggleMenu();
-// menuSpy();
+// parallax();
+flipCard();
+toggleMenu();
+menuSpy();
 // initMap();
